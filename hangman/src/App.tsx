bc5e3bd9928d6c58b23691716fd7a77cd17c5800
mustafa <msolmaz83@gmail.com>
2023-10-26 00:24:31 +0200

    css_okey

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,13 +1,32 @@
 
+import data from './data.json'
+import {useState} from 'react'
+import HangmanDr from './components/HangmanDr'
+import HangmanWort from './components/HangmanWort'
+import Keyboard from './components/Keyboard'
 
 function App() {
-  
+    const  word =  data[Math.floor(Math.random()*data.length)]
+    //console.log(word)
+     const [hang,setHang] = useState(word)
+    //console.log(hang)
+
 
   return (
-    <>
-    hallo
+    <div style={{
+      maxWidth:'800px',
+      display:'flex',
+      flexDirection:'column',
+      gap:'2rem',
+      margin:'0 auto',
+      alignItems:'center'
+    }}>
+    <div style={{fontSize:'2rem' ,textAlign:'center' }}>Lose Win</div>
+    <HangmanDr/>
+    <HangmanWort/>
+    <Keyboard/>
        
-    </>
+    </div>
   )
 }
 
---
+++ hangman/src/components/HangmanWort.tsx
@@ -0,0 +1,9 @@
+
+
+const HangmanWort = () => {
+  return (
+    <div>HangmanWort</div>
+  )
+}
+
+export default HangmanWort
